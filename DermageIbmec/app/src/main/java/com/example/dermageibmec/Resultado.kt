c61441f4b2134f1c3bd82ab5e706a16c4cb62adc
Victor Hugo <victorhrapinto@gmail.com>
2023-06-06 15:41:02 -0300

    reenvio projeto

---
+++ DermageIbmec/app/src/main/java/com/example/dermageibmec/Resultado.kt
@@ -0,0 +1,10 @@
+package com.example.dermageibmec
+
+class Resultado {
+    companion object{
+        var resp1:Int = 0
+        var resp2: MutableList<Int> = mutableListOf()
+        var resp3: MutableList<Int> = mutableListOf()
+        var resp4: Int = 0
+    }
+}
